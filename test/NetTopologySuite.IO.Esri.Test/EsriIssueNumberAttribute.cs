a25810e1dbf32c1f6a920c16663d2d2a26897da4
Kuba Szostak <kuba@szostak.io>
2022-07-16 11:38:13 +0200

    Add EsriIssueNumberAttribute

---
+++ test/NetTopologySuite.IO.Esri.Test/EsriIssueNumberAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NetTopologySuite.IO.Esri.Test
+{
+    /// <summary>
+    /// The issue number used in this test (or fixture) refers to an issue on
+    /// https://github.com/NetTopologySuite/NetTopologySuite.IO.Esri, created
+    /// after this project was moved to new repository (and thus, it got its own
+    /// set of issue numbers).
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    public sealed class EsriIssueNumberAttribute : PropertyAttribute
+    {
+        public EsriIssueNumberAttribute(int issueNumber)
+            : base("NetTopologySuite.IO.Esri issue", issueNumber)
+        {
+        }
+    }
+}
