948412f6e87569de956a8565b29b836b9bd9c6a6
Kuba Szostak <kuba@szostak.io>
2022-07-16 12:54:07 +0200

    Add PathTo() TestShapefiles.

--- test/NetTopologySuite.IO.Esri.Test/TestShapefiles.cs
+++ test/NetTopologySuite.IO.Esri.Test/TestShapefiles.cs
@@ -3,5 +3,10 @@ using System.IO;
 
 internal sealed class TestShapefiles
 {
-    public static readonly string TestShapefilesDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestShapefiles");
+    public static readonly string Directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestShapefiles");
+
+    public static string PathTo(string shpName)
+    {
+        return Path.Combine(Directory, shpName);
+    }
 }
