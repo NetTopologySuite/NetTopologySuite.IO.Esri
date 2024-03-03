721bd8a2c5a9b6b22320054fcc20477aaa53d590
Kuba Szostak <kuba@szostak.io>
2024-03-03 12:23:13 +0100

    Make SHP path linux compatible.

--- test/NetTopologySuite.IO.Esri.Test/Issues/Issue033.cs
+++ test/NetTopologySuite.IO.Esri.Test/Issues/Issue033.cs
@@ -16,7 +16,7 @@ internal class Issue033
     [Test]
     public void WriteAllFeatures_NullAttributeValue()
     {
-        string shpPath = TestShapefiles.PathTo(@"Issues\033\sample\ne_10m_admin_0_countries_fra.shp");
+        string shpPath = TestShapefiles.PathTo(@"Issues/03/sample/ne_10m_admin_0_countries_fra.shp");
         string shpCopyPath = TestShapefiles.GetTempShpPath();
 
         var config = new ShapefileReaderOptions()
