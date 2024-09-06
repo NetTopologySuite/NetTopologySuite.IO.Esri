d1f50ed39a925e810c3c0cb4f438ccf83ae729cf
DiegoNetoMartins <dneto.martins@gmail.com>
2024-09-06 09:08:53 -0300

    Add unit test to validate modification related to DbfNumericDecimalField

---
+++ test/NetTopologySuite.IO.Esri.Test/Issues/Issue057.cs
@@ -0,0 +1,30 @@
+﻿using NetTopologySuite.IO.Esri.Shapefiles.Readers;
+using NUnit.Framework;
+
+namespace NetTopologySuite.IO.Esri.Test.Issues;
+
+/// <summary>
+/// https://github.com/NetTopologySuite/NetTopologySuite.IO.Esri/issues/57
+/// </summary>
+internal class Issue057
+{
+    [Test]
+    public void Decimal0_OpenShapefile()
+    {
+        var shpPath = TestShapefiles.PathTo("Issues/057/deter-nf-deter-public.shp");
+        var options = new ShapefileReaderOptions
+        {
+            GeometryBuilderMode = GeometryBuilderMode.FixInvalidShapes
+        };
+        using var shpReader = Shapefile.OpenRead(shpPath, options);
+
+        Assert.AreEqual(shpReader.ShapeType, ShapeType.Polygon);
+
+        while (shpReader.Read())
+        {
+            var dosis = shpReader.Fields["AREA_KM"].Value.ToString();
+            Assert.IsNotEmpty(dosis);
+            Assert.IsFalse(shpReader.Geometry.IsEmpty);
+        }
+    }
+}
